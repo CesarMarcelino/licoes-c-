6aed3289dc68fc04817712b6e8e7e8d28063a990
CesarMarcelino <cmarcelinonac@icloud.com>
2024-08-11 18:38:51 -0300

    LicoesAlteradas

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Console.WriteLine("+----Minhas Frutas Preferidas----+");
+Console.WriteLine("|                                |");
+Console.WriteLine("|           1. Manga             |");
+Console.WriteLine("|           2. Uva               |");
+Console.WriteLine("|           3. Morango           |");
+Console.WriteLine("|           4. Maçã              |");
+Console.WriteLine("|           5. Pessego           |");
+Console.WriteLine("|                                |");
+Console.WriteLine("+--------------------------------+");
