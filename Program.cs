fa6f0d7fcba7fd0f0fcedfe520ef806cb449cc15
CesarMarcelino <cmarcelinonac@icloud.com>
2024-08-11 17:58:21 -0300

    musicas

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿Console.BackgroundColor = ConsoleColor.DarkRed;
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("The Box-Roddy Ricch");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("Pullin' out the coupe at the lot");
+Console.WriteLine("Told 'em fuck 12, fuck SWAT");
+Console.WriteLine("Bustin' all the bells out the box");
+Console.WriteLine("I just hit a lick with the box");
+Console.WriteLine("Had to put the stick in a box, mmh");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("Pour up the whole damn seal, I'ma get lazy");
+Console.WriteLine("I got the mojo deals, we been trappin' like the '80s");
+Console.WriteLine("She sucked a nigga soul, gotta Cash App");
+Console.WriteLine("Told 'em wipe a nigga nose, say slatt, slatt");
+Console.WriteLine("I won't never sell my soul, and I can back that");
+Console.WriteLine("And I really wanna know, where you at, at?");
+Console.WriteLine();
+
+Console.WriteLine("I was out back, where the stash at?");
+Console.WriteLine("Cruise the city in a bulletproof Cadillac (skrrt)");
+Console.WriteLine("'Cause I know these niggas after where the bag at (yeah)");
+Console.WriteLine("Gotta move smarter, gotta move harder");
+Console.WriteLine("Niggas try to get me for my water");
+Console.WriteLine("I'll lay his ass down on my son, or my daughter");
+Console.WriteLine("I had the Draco with me, Dwayne Carter");
+Console.WriteLine("Lotta niggas out here playin', ain't ballin'");
+Console.WriteLine();
+
+Console.WriteLine("I done out my whole arm in the rim, Vince Carter (yeah)");
+Console.WriteLine("And I know probably get a key for the quarter");
+Console.WriteLine("Shawty barely seen in double C's, I bought 'em");
+Console.WriteLine("Got a bitch that's looking like Aaliyah, she a model");
+Console.WriteLine("I got the pink slip, all my whips is key-less");
+Console.WriteLine("Compton, I'm about to get the key to the city");
+Console.WriteLine("Patek like the sea, forgive me");
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("Pullin' out the coupe at the lot");
+Console.WriteLine("Told 'em fuck 12, fuck SWAT");
+Console.WriteLine("Bustin' all the bells out the box");
+Console.WriteLine("I just hit a lick with the box");
+Console.WriteLine("Had to put the stick in a box, mmh");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("Pour up the whole damn seal, I'ma get lazy");
+Console.WriteLine("I got the mojo deals, we been trappin' like the '80s");
+Console.WriteLine("She sucked that nigga soul, gotta Cash App");
+Console.WriteLine("Told 'em wipe a nigga nose, say slatt, slatt");
+Console.WriteLine("I won't never sell my soul, and I can back that");
+Console.WriteLine("And I really wanna know, where you at, at? Huh");
+Console.WriteLine();
+
+Console.WriteLine("Ha-ha-ha, I been movin' 'em out");
+Console.WriteLine("If Steelo with me, then he got the blues in the pouch (yeah)");
+Console.WriteLine("Took her to the forrest, put wood in her mouth");
+Console.WriteLine("Bitch don't wear no shoes in my house");
+Console.WriteLine("The private I'm flyin' in, I never wanna fly again");
+Console.WriteLine("I'll take my chances in traffic (yeah)");
+Console.WriteLine("She suckin' on dick, no hands with it");
+Console.WriteLine("I just made the Rollie plain like a landing-strip");
+Console.WriteLine();
+
+Console.WriteLine("I'm a 2020 president candidate");
+Console.WriteLine("I done put a hundred bands on Zimmerman, shit");
+Console.WriteLine("I been movin' real gangsta', so that's why she pick a Crip");
+Console.WriteLine("Shawty call me Crisco, 'cause I pop my shit");
+Console.WriteLine("Got it out the mud, there's nothin' you can tell me, yeah");
+Console.WriteLine("When I had the drugs, I was street-wealthy, yeah");
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("Pullin' out the coupe at the lot");
+Console.WriteLine("Told 'em fuck 12, fuck SWAT");
+Console.WriteLine("Bustin' all the bells out the box");
+Console.WriteLine("I just hit a lick with the box");
+Console.WriteLine("Had to put the stick in a box, mmh");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("Pour up the whole damn seal, I'ma get lazy");
+Console.WriteLine("I got the mojo deals, we been trappin' like the '80s");
+Console.WriteLine("She sucked a nigga soul, gotta Cash App");
+Console.WriteLine("Told 'em wipe a nigga nose, say slatt, slatt");
+Console.WriteLine("I won't never sell my soul, and I can back that");
+Console.WriteLine("And I really wanna know, where you at, at?");
